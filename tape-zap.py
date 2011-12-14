bc3bd3b7e3ad6f4681385091a1089f084363ee1d
Greg Baker <gregb@ifost.org.au>
2011-12-14 01:17:57 +0000

    Fixes to work on Windows.

--- tape-zap.py
+++ tape-zap.py
@@ -14,11 +14,16 @@ Almost no error checking is done.
 
 It needs to run on a machine which has the cell console software on it,
 and as a user who has at least the rights to run omnimm.
+
+(c) Greg Baker 2010-2011 <gregb@ifost.org.au>
+
+$Id$
 """
 
 import os
 import sys
 import string
+import subprocess
 
 if len(sys.argv) == 1:
   sys.exit("Please supply a location you would like the tapes to be moved to")
@@ -28,14 +33,16 @@ dest_location = string.join(sys.argv[1:]," ")
 print "Marking tapes as being in location: ",dest_location
 # Perhaps I should check that this is a valid location?
 
-if sys.platform != 'nt':
+if sys.platform != 'win32':
   omnimm = '/opt/omni/bin/omnimm'
 else:
-  for possibility in ["C:\\Program Files\\Omniback\bin\omnimm","D:\\Program Files\\Omniback\bin\omnimm"]:
+  omnimm = None
+  for possibility in ["C:\\Program Files\\OmniBack\\bin\\omnimm.exe","D:\\Program Files\\Omniback\\bin\\omnimm.exe"]:
     if os.path.exists(possibility):
       omnimm = possibility
       break 
-  sys.exit("Can't find omnimm program")
+  if omnimm is None:
+    sys.exit("Can't find omnimm program")
 
 while 1:
   print "Zap or enter the label of the tape (x to exit): ",
@@ -47,11 +54,11 @@ while 1:
   # Assume that no-one puts " or funny chars into 
   # If we were sure to be on unix, I'd just use os.fork and os.exevc so
   # that no quoting was needed
-  command = '"'+omnimm+'" -modify_medium "'+tape_name+'" "'+tape_name+'" "'+dest_location+'"'
-  result = os.system(command)
+  command = [omnimm,"-modify_medium",tape_name,tape_name,dest_location]
+  result = subprocess.call(command)
   if result == 0:
     print "\a",
   else:
     print "\a\a"
-    sys.stderr.write("Command failed: " + command)
+    sys.stderr.write("Command failed: " + string.join(" ",command))
 
